6e1c5174b8338e2719a83f35e4879b2431dc379a
ExcaliburZero <cwellsny@nycap.rr.com>
2015-10-22 22:03:45 -0400

    Added some basic usage of ArrayLists
    Added a few basic usages of the ArrayList class.

--- src/javaarraylisttest/Main.java
+++ src/javaarraylisttest/Main.java
@@ -5,6 +5,8 @@
  */
 package javaarraylisttest;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Christopher Wells <cwellsny@nycap.rr.com>
@@ -15,7 +17,22 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		// Create an empty ArrayList
+		ArrayList testArray = new ArrayList();
+		
+		// Fill the ArrayList with the numbers 0 through 12
+		for (int i = 0; i < 12; i++) {
+			testArray.add(i);
+		}
+		
+		// Print out the contents of the ArrayList
+		System.out.println("Contents: " + testArray);
+		
+		// Clear the contents of the ArrayList
+		testArray.clear();
+		
+		// Print out the empty ArrayList
+		System.out.println("Contents: " + testArray);
 	}
 	
 }
